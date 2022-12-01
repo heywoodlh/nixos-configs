e19ac4b081ead962180529862f6638c3dc72c837
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-12-01 14:41:33 -0700

    external displays working with Razer Blade 15!
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- workstation/razer-blade-15.nix
+++ workstation/razer-blade-15.nix
@@ -11,20 +11,11 @@
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
 
-  disabledModules = [ "hardware/video/nvidia.nix" ];
-
-  imports =
-    [ # sudo nix-channel --add https://github.com/GoogleBot42/nixpkgs/archive/refs/heads/master.tar.gz nvidia-reverse-prime 
-      <nvidia-reverse-prime/nixos/modules/hardware/video/nvidia.nix>
-    ];
-
   hardware.nvidia = {
     powerManagement.enable = true;
     modesetting.enable = true;
     prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
-      reverse_sync.enable = true;
+      sync.enable = true;
       nvidiaBusId = "PCI:1:0:0";
       intelBusId = "PCI:0:2:0";
     };

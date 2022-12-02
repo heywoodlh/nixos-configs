bf981d2a5065390eecb37619a3d1758b626125b6
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-12-02 10:45:29 -0700

    improve bluetooth output/input
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- workstation/desktop.nix
+++ workstation/desktop.nix
@@ -46,9 +46,39 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            # mSBC is not expected to work on all headset + adapter combinations.
+            "bluez5.msbc-support" = true;
+            # SBC-XQ is not expected to work on all headset + adapter combinations.
+            "bluez5.sbc-xq-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
   };
+
+  ## Experimental flag allows battery reporting for bluetooth devices
+  systemd.services.bluetooth.serviceConfig.ExecStart = [
+    ""
+    "${pkgs.bluez}/libexec/bluetooth/bluetoothd --experimental"
+  ];
   
   # Android debugging
   programs.adb.enable = true;

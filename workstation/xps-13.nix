9895ad7e30ae9a7473fb5a5105bd761f500cdefc
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-12-30 20:02:56 -0700

    added dell xps config (hardware accelerated video)
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

---
+++ workstation/xps-13.nix
@@ -0,0 +1,16 @@
+# Config specific to Dell XPS 13
+
+{ config, pkgs, lib, ... }:
+
+{
+  ## Hardware acceleration for Intel graphics
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+}

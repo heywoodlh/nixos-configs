2fec160ab481ad72879321eb7ca0bc41293373ab
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-01-13 16:53:42 -0700

    updated darwin-noninteractive.sh
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- tools/darwin/darwin-noninteractive.sh
+++ tools/darwin/darwin-noninteractive.sh
@@ -108,6 +108,7 @@ fi
 
 # Run the remaining commands as $username
 sudo -i -u ${username} bash << EOF
+    export HOME=/Users/${username}
     cd /Users/${username}
     # If homebrew is installed, make sure that shellenv is evaluated
     if test -e ${homebrew_bin_path} > /dev/null

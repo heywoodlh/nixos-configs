d77a3261da784471b09485ed9418f5d3f1bf6a55
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-04 13:27:43 -0700

    updated nix-kube-1 to be kubernetes master
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/hosts/nix-kube-1/configuration.nix
+++ nixos/hosts/nix-kube-1/configuration.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
-{
+let
+  kubeMasterIP = "10.0.50.41";
+  kubeMasterHostname = "nix-kube-1.kube";
+  kubeMasterAPIServerPort = 6443;
+in {
   imports =
   [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../server.nix
-    ../../../roles/minikube.nix
   ];
 
   # Bootloader.
@@ -47,5 +50,36 @@
     "resolv.conf".text = "nameserver 10.50.50.1\n";
   };
 
+  # Kubernetes master
+  # resolve master hostname
+  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+
+  # packages for administration tasks
+  environment.systemPackages = with pkgs; [
+    kompose
+    kubectl
+    kubernetes
+  ];
+
+  # allow kubernetes port
+  networking.firewall.allowedTCPPorts = [ kubeMasterAPIServerPort ];	
+
+  services.kubernetes = {
+    roles = ["master" "node"];
+    masterAddress = kubeMasterHostname;
+    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+    easyCerts = true;
+    apiserver = {
+      securePort = kubeMasterAPIServerPort;
+      advertiseAddress = kubeMasterIP;
+    };
+
+    # use coredns
+    addons.dns.enable = true;
+
+    # needed if you use swap
+    #kubelet.extraOpts = "--fail-swap-on=false";
+  };
+  
   system.stateVersion = "22.11";
 }

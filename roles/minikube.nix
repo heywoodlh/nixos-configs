0db295c794e82354a2629213920cfdc996d8172a
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-07 16:57:24 -0700

    updated minikube config
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/minikube.nix
+++ roles/minikube.nix
@@ -14,6 +14,7 @@ let
 in {  
   # Minikube package
   environment.systemPackages = with pkgs; [
+    conntrack
     kubectl
     minikube
   ];
@@ -29,7 +30,7 @@ in {
       pkgs.bash
       pkgs.docker
       pkgs.iptables
-      pkgs.libnetfilter_conntrack
+      pkgs.conntrack
       pkgs.minikube 
       pkgs.systemd 
     ];

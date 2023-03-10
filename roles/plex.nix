418989cb0becfd3ff8948d1821f0d1cb7237f1e2
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-10 14:24:28 -0700

    updated plex.nix to use unprivileged media user with UID 1050
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/plex.nix
+++ roles/plex.nix
@@ -8,24 +8,22 @@
   users.users.media = {
     group = "media";
     isSystemUser = true;
+    uid = 1050;
   };
 
   fileSystems."/media/services" = {
     device = "10.0.50.50:/media/disk2/services/media";
     fsType = "nfs";
-    options = [ "rw,hard,intr" "noatime" "vers=4" "minorversion=0" "sec=sys" "uid=media" ];
   };
 
   fileSystems."/media/movies-1" = {
     device = "10.0.50.50:/media/disk1/movies";
     fsType = "nfs";
-    options = [ "rw,hard,intr" "noatime" "vers=4" "minorversion=0" "sec=sys" "uid=media" ];
   };
 
   fileSystems."/media/tv-1" = {
     device = "10.0.50.50:/media/disk1/tv";
     fsType = "nfs";
-    options = [ "rw,hard,intr" "noatime" "vers=4" "minorversion=0" "sec=sys" "uid=media" ];
   };
 
   services.plex = {

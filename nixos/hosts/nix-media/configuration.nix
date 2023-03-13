e4f6c8cd9d8d1ce8cde3f5256573dc8d63c810cf
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-13 11:37:03 -0600

    removed kind from nix-media
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/hosts/nix-media/configuration.nix
+++ nixos/hosts/nix-media/configuration.nix
@@ -5,7 +5,6 @@
   [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../server.nix
-    ../../../roles/kind.nix
     ../../../roles/plex.nix
     ../../../roles/freshrss.nix
   ];

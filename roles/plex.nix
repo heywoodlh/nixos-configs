4a847b8a14712880abc602dbb91e8e652837b965
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-13 12:11:47 -0600

    updated nfs options for plex and freshrss
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/plex.nix
+++ roles/plex.nix
@@ -15,16 +15,19 @@
   fileSystems."/media/services" = {
     device = "10.0.50.50:/media/disk2/services/media";
     fsType = "nfs";
+    options = [ "rw" "bg" "hard" "rsize=1048576" "wsize=1048576" "tcp" "timeo=600" ];
   };
 
   fileSystems."/media/movies-1" = {
     device = "10.0.50.50:/media/disk1/movies";
     fsType = "nfs";
+    options = [ "rw" "bg" "hard" "rsize=1048576" "wsize=1048576" "tcp" "timeo=600" ];
   };
 
   fileSystems."/media/tv-1" = {
     device = "10.0.50.50:/media/disk1/tv";
     fsType = "nfs";
+    options = [ "rw" "bg" "hard" "rsize=1048576" "wsize=1048576" "tcp" "timeo=600" ];
   };
 
   services.plex = {

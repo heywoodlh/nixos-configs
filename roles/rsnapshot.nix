bf9c649f9ac6cd3eefa1f3d75206460fa3fd9ce2
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-01 16:16:55 -0600

    added rsnapshot role
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

---
+++ roles/rsnapshot.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  services.rsnapshot = {
+    enable = true;
+    enableManualRsnapshot = true;
+    cronIntervals = {
+      daily = "0 3 * * *";
+      hourly = "0 * * * *";
+      weekly = "0 1 * * 1";
+      monthly = "0 1 1 * *";
+    };
+    extraConfig = ''
+      snapshot_root   /media/backups/
+      retain  hourly  24
+      retain  daily   7
+      retain  weekly  4
+      retain  monthly 6
+
+      backup      root@ct-1.wireguard:/etc/     ct-1/etc/
+      backup      root@ct-1.wireguard:/home/     ct-1/home/
+      backup      root@ct-1.wireguard:/opt/     ct-1/opt/
+      backup      root@ct-1.wireguard:/root/     ct-1/root/
+    '';
+  };
+}

366b6171a5cdda948f11881889e5a373d7808c5b
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-04 11:44:53 -0600

    added auto-update cron
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

---
+++ roles/auto-update.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+let
+  gotify-setup = pkgs.writeScriptBin "nixos-switch-auto" ''
+    #!/usr/bin/env bash
+    # This script automatically updates my flake locally
+
+    [[ ! -d /opt/nixos-configs ]] && git clone https://github.com/heywoodlh/nixos-configs /opt/nixos-configs
+    # If /opt/nixos-configs is out of date, then run git pull
+    if git -C /opt/nixos-configs remote show origin  | grep -iq "out of date"
+    then
+        git -C /opt/nixos-configs pull origin master
+        nixos-rebuild switch --flake /opt/nixos-configs#$(hostname) --impure && export AUTO_UPDATE=1
+
+        if [[ $AUTO_UPDATE == 1 ]]
+        then
+            echo "Auto updated $(hostname) to $(nixos-version)" | gotify push -t "NixOS update: $(hostname)" 
+        else
+            echo "Failed to auto update $(hostname)" | gotify push -t "NixOS update failed: $(hostname)"
+        fi 
+    fi
+  '';
+
+in {
+  # Ensure that dependencies are installed
+  environment.systemPackages = with pkgs; [ 
+    bash
+    git
+    nixos-switch-auto
+  ];
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "*/5 * * * *      root    nixos-switch-auto"
+    ];
+  };
+}

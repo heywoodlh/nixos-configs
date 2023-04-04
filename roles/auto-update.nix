a9cbe2246b8a9fb9622da55af4597972febddbc0
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-04 11:54:33 -0600

    updated auto-update.nix
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/auto-update.nix
+++ roles/auto-update.nix
@@ -5,7 +5,20 @@ let
     #!/usr/bin/env bash
     # This script automatically updates my flake locally
 
-    [[ ! -d /opt/nixos-configs ]] && git clone https://github.com/heywoodlh/nixos-configs /opt/nixos-configs
+    [[ ! -d /opt/nixos-configs ]] && git clone https://github.com/heywoodlh/nixos-configs /opt/nixos-configs && export NIXOS_DOWNLOAD=1
+
+    # If NIXOS_DOWNLOAD = 1, then run nixos-rebuild switch
+    if [[ $NIXOS_DOWNLOAD == 1 ]]
+    then
+        nixos-rebuild switch --flake /opt/nixos-configs#$(hostname) --impure && export AUTO_UPDATE=1
+        if [[ $AUTO_UPDATE == 1 ]]
+        then
+            echo "Auto updated $(hostname) to $(nixos-version)" | gotify push -t "NixOS update: $(hostname)" 
+        else
+            echo "Failed to auto update $(hostname)" | gotify push -t "NixOS update failed: $(hostname)"
+        fi 
+    fi
+
     # If /opt/nixos-configs is out of date, then run git pull
     if git -C /opt/nixos-configs remote show origin  | grep -iq "out of date"
     then

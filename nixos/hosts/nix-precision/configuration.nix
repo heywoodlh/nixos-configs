a5ba77cbf5c11433e3d7b325d52bd05448364c53
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-20 13:34:29 -0600

    updated nix-precision configs
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/hosts/nix-precision/configuration.nix
+++ nixos/hosts/nix-precision/configuration.nix
@@ -30,13 +30,13 @@
   };
 
   # Virtual machine media
-  filesystems."/media/virtual-machines" = {
+  fileSystems."/media/virtual-machines" = {
     device = "/dev/disk/by-uuid/2fa5a6c4-b938-4853-844d-c85a77ae33e7";
     fsType = "ext4";
     options = [ "rw" "relatime" ];
   };
 
-  filesystems."/media/virtual-machines-2" = {
+  fileSystems."/media/virtual-machines-2" = {
     device = "/dev/disk/by-uuid/b1d9e75f-df2c-4ec0-a691-71aebf100cd6";
     fsType = "ext4";
     options = [ "discard" "noatime" "commit=600" "errors=remount-ro" ];

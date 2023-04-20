d84771ec9fa8e6cccba4f537cbe296b23bb56234
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-20 15:55:19 -0600

    updated squid to not proxy docker
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/roles/squid-client.nix
+++ nixos/roles/squid-client.nix
@@ -4,6 +4,6 @@
   networking.proxy = {
     httpProxy = "http://100.113.9.57:3128";
     httpsProxy = "http://100.113.9.57:3128";
-    noProxy = "localhost,127.0.0.1";
+    noProxy = "localhost,127.0.0.1,docker.io";
   };
 }

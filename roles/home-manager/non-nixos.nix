9807cd360281c6766848897a68cba76651ce77e2
Spencer Heywood <lamar.heywood@ll.mit.edu>
2023-04-25 13:04:48 -0600

    removed iosevka font from nerdfonts
    Signed-off-by: Spencer Heywood <lamar.heywood@ll.mit.edu>

--- roles/home-manager/non-nixos.nix
+++ roles/home-manager/non-nixos.nix
@@ -2,6 +2,6 @@
 
 {
   home.packages = [
-    (pkgs.nerdfonts.override { fonts = [ "Hack" "DroidSansMono" "Iosevka" "JetBrainsMono" ]; })
+    (pkgs.nerdfonts.override { fonts = [ "Hack" "DroidSansMono" "JetBrainsMono" ]; })
   ];
 }

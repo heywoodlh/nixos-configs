aa30a15744ee4ad4ec86a62fca804d23a8a31f45
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-28 16:41:45 -0600

    updated squid-client.nix
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/roles/squid-client.nix
+++ nixos/roles/squid-client.nix
@@ -5,6 +5,6 @@
     default = "http://100.113.9.57:3128";
     httpProxy = "http://100.113.9.57:3128";
     httpsProxy = "http://100.113.9.57:3128";
-    noProxy = "localhost,127.0.0.1,docker.io,*.docker.io,*.docker.com,*.nixos.org,github.com,githubusercontent.com";
+    noProxy = "localhost,127.0.0.1,docker.io,*.docker.io,*.docker.com,*.nixos.org,github.com,githubusercontent.com,ghcr.io,*.ghcr.io";
   };
 }

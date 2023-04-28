fa05d4a671cf02c0933f7ba64d81f2a4e09dcb97
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-04-28 17:02:54 -0600

    added serge
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

---
+++ nixos/roles/serge.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      serge = {
+        image = "ghcr.io/nsarrazin/serge:latest";
+        autoStart = true;
+        ports = ["8008:8008"];
+        volumes = [
+          "/opt/serge/db:/data/db"
+          "/opt/serge/weights:/usr/src/app/weights/"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+      };
+    };
+  };
+}

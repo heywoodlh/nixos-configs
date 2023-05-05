0e6916df8cd812c0d5ea34d526f615747247a7c4
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-05-05 08:40:42 -0600

    disabled vscode on non-desktop machines
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/home-manager/linux/no-desktop.nix
+++ roles/home-manager/linux/no-desktop.nix
@@ -2,31 +2,36 @@
 
 # Mainly, this file removes things we don't need from desktop builds
 {
-  home-manager.users.heywoodlh.dconf.settings = lib.mkForce {
-  };
+  home-manager.users.heywoodlh = {
+    dconf.settings = lib.mkForce {};
 
-  home-manager.users.heywoodlh.home.packages = with pkgs; lib.mkForce [
-    curl
-    glow
-    htop
-    jq
-    lefthook
-    lima
-    pandoc
-    rbw
-    tcpdump
-    tmux
-    tree
-    w3m
-    vim
-    zsh
-  ];
+    home.packages = with pkgs; lib.mkForce [
+      curl
+      glow
+      htop
+      jq
+      lefthook
+      lima
+      pandoc
+      rbw
+      tcpdump
+      tmux
+      tree
+      w3m
+      vim
+      zsh
+    ];
+  
+    programs.alacritty = lib.mkForce {
+      enable = false;
+    };
+  
+    programs.firefox = lib.mkForce {
+      enable = false;
+    };
 
-  home-manager.users.heywoodlh.programs.alacritty = lib.mkForce {
-    enable = false;
-  };
-
-  home-manager.users.heywoodlh.programs.firefox = lib.mkForce {
-    enable = false;
+    programs.vscode = lib.mkForce {
+      enable = false;
+    };
   };
 }

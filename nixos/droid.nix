548342983dca868a1150cdb386921d7696633e45
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-05-07 22:11:07 -0600

    added extraSpecialArgs for nix-on-droid
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/droid.nix
+++ nixos/droid.nix
@@ -1,12 +1,7 @@
 { config, pkgs, lib, home-manager, nur, nix-on-droid, ... }:
 
 {
-  imports = [
-    ../roles/home-manager/linux/no-desktop.nix
-  ];
-
-  # Home-manager config
-  home-manager.config = ../roles/home-manager/linux.nix;
+  home-manager.config = ../roles/home-manager/linux.nix { inherit config; inherit pkgs; inherit home-manager; inherit lib; };
   # Read Nix-on-Droid changelog before changing this value
   system.stateVersion = "22.11";
 }

40c8f9e9c45d5db9c0ec666c450e9fcf45e918ee
Spencer Heywood <lamar.heywood@ll.mit.edu>
2023-05-17 12:00:54 -0600

    better privacy/less notifications in vscode
    Signed-off-by: Spencer Heywood <lamar.heywood@ll.mit.edu>

--- roles/home-manager/base.nix
+++ roles/home-manager/base.nix
@@ -375,13 +375,38 @@
       }
     ];
     userSettings = {
+      "Lua.telemetry.enable" = false;
+      "clangd.checkUpdates" = false;
+      "code-runner.enableAppInsights" = false;
+      "docker-explorer.enableTelemetry" = false;
       "editor.fontFamily" = "'JetBrainsMono Nerd Font Mono', 'monospace', 'Droid Sans Mono', 'monospace', 'Droid Sans Fallback'";
+      "extensions.ignoreRecommendations" = true;
+      "gitlens.showWelcomeOnInstall" = false;
+      "gitlens.showWhatsNewAfterUpgrades" = false;
+      "java.help.firstView" = "none";
+      "java.help.showReleaseNotes" = false;
+      "julia.enableTelemetry" = false;
+      "kite.showWelcomeNotificationOnStartup" = false;
+      "liveServer.settings.donotShowInfoMsg" = true;
+      "material-icon-theme.showWelcomeMessage" = false;
+      "pros.showWelcomeOnStartup" = false;
+      "pros.useGoogleAnalytics" = false;
+      "redhat.telemetry.enabled" = false;
       "remote.SSH.useLocalServer" = false;
+      "rpcServer.showStartupMessage" = false;
+      "shellcheck.disableVersionCheck" = true;
+      "sonarlint.disableTelemetry" = true;
+      "telemetry.enableCrashReporter" = false;
       "telemetry.enableTelemetry" = false;
+      "telemetry.telemetryLevel" = "off";
       "terminal.integrated.macOptionIsMeta" = true;
       "terminal.integrated.shellIntegration.enabled" = true;
+      "terraform.telemetry.enabled" = false;
+      "update.showReleaseNotes" = false;
       "vim.useSystemClipboard" = true;
+      "vsicons.dontShowNewVersionMessage" = true;
       "workbench.colorTheme" = "Nord";
+      "workbench.welcomePage.walkthroughs.openOnInstall" = false;
     };
   };
 

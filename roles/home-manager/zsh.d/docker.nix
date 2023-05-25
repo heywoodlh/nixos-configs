13b7d50d3809bc6a95b2298ce2c5f5b365d71368
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-05-25 12:14:08 -0600

    updated amass function to point to docker.io as registry
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/home-manager/zsh.d/docker.nix
+++ roles/home-manager/zsh.d/docker.nix
@@ -1,5 +1,5 @@
 ''
-  alias amass='mkdir -p ~/.local/amass && docker run -it --rm -v ~/.local/amass:/.config/amass/ caffix/amass $@'
+  alias amass='mkdir -p ~/.local/amass && docker run -it --rm -v ~/.local/amass:/.config/amass/ docker.io/caffix/amass $@'
 
   alias msfconsole='mkdir -p ~/.local/metasploit && docker run -it --rm --net host -v ~/.local/metasploit/:/root/.msf4 -w /root/session -v $(pwd):/root/session docker.io/heywoodlh/metasploit msfconsole $@'
 

3b3247e73216cfa8eb1325b263b003a0e4409f40
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-05-31 09:53:29 -0600

    fixed trailing spaces in base.nix; set alacritty to use block cursor
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- roles/home-manager/base.nix
+++ roles/home-manager/base.nix
@@ -75,10 +75,10 @@
   ];
 
   # Import nur as nixpkgs.overlays
-  nixpkgs.overlays = [ 
-    nur.overlay 
+  nixpkgs.overlays = [
+    nur.overlay
   ];
-  
+
   programs.alacritty = {
     enable = true;
     settings = {
@@ -91,6 +91,10 @@
         cursor = {
           text = "#2e3440";
           cursor = "#d8dee9";
+          style = {
+            shape = "Block";
+          };
+          blinking = "On";
         };
         vi_mode_cursor = {
           text = "#2e3440";
@@ -141,7 +145,7 @@
           white = "#aeb3bb";
         };
       };
-      
+
       font = {
         normal = {
           family = "JetBrainsMono Nerd Font Mono";
@@ -169,7 +173,7 @@
         program = ".nix-profile/bin/tmux";
       };
     };
-  }; 
+  };
 
   programs.git = {
     enable = true;
@@ -220,62 +224,61 @@
       # Change default prefix key to C-a, similar to screen
       unbind-key C-b
       set-option -g prefix C-a
-      
+
       # Enable 24-bit color support
       set-option -ga terminal-overrides ",xterm-termite:Tc"
-      
+
       # Start window indexing at one
       set-option -g base-index 1
-      
+
       # Use vi-style key bindings in the status line, and copy/choice modes
       set-option -g status-keys vi
       set-window-option -g mode-keys vi
-      
+
       # Large scrollback history
       set-option -g history-limit 10000
-      
+
       # Xterm Keys on
       set-window-option -g xterm-keys on
-      
+
       # Set 256 colors
       set -g default-terminal "screen-256color"
-      
+
       # Set escape time to zero
       set -sg escape-time 0
-      
+
       # move between panes with vim-like motions
       bind h select-pane -L
       bind j select-pane -D
       bind k select-pane -U
       bind l select-pane -R
-  
+
       unbind % # Split vertically
       unbind '"' # Split horizontally
-  
+
       bind-key | split-window -h -c "#{pane_current_path}"
       bind-key - split-window -v -c "#{pane_current_path}"
-      
-      
+
       # Synchronizing panes
       #bind-key y set-window-option synchronize-panes
-      
+
       # SSH to Host
       bind-key S command-prompt -p ssh: "new-window -n %1 'ssh %1'"
       # Mosh to Host
       bind-key M command-prompt -p mosh: "new-window -n %1 'mosh %1'"
-      
+
       # Mouse mode
       set -g mouse on
-      
+
       # Tmux Scrolling
       bind -n WheelUpPane   select-pane -t= \; copy-mode -e \; send-keys -M
       bind -n WheelDownPane select-pane -t= \;                 send-keys -M
-      
+
       bind a send-prefix
-      
+
       # vim-selection
       bind-key -T copy-mode-vi 'v' send-keys -X begin-selection
-      
+
       # Status bar
       set -g status off
       #set -g status-left "#[fg=black,bg=blue,bold] #S #[fg=blue,bg=black,nobold,noitalics,nounderscore]"
@@ -304,30 +307,30 @@
       let g:airline_theme='nord'
       " Enable ale with airline
       let g:airline#extensions#ale#enabled = 1
-  
+
       syntax on
-      
+
       colorscheme nord
-      
+
       filetype plugin indent on
       let g:mapleader = "\<Space>"
       let g:maplocalleader = ','
       nnoremap <silent> <leader>      :<c-u>WhichKey '<Space>'<CR>
       nnoremap <silent> <localleader> :<c-u>WhichKey  ','<CR>
       map <C-n> :NERDTreeToggle<CR>
-      
+
       set number
-      
+
       " Start Nerdtree only if width is greater than or equal to 80 columns
       "if winwidth(0) >= 100
       "	let g:NERDTreeWinSize=winwidth(0)/6
-      "	autocmd VimEnter * NERDTree | wincmd p	
+      "	autocmd VimEnter * NERDTree | wincmd p
       "endif
       " Start NERDTree when Vim starts with a directory argument.
       autocmd StdinReadPre * let s:std_in=1
       autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
           \ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
-      
+
       " Vim-indentline config
       let g:indentLine_char = '⦙'
       set shiftwidth=4
@@ -338,7 +341,7 @@
         autocmd!
         autocmd FileType dockerfile let g:indentLine_enabled = 0
       augroup END
-      
+
       " Custom keybinds
       nnoremap zz :wa <bar> :qa!<CR>
       nnoremap zq <bar> :qa!<CR>
@@ -346,51 +349,51 @@
       map <silent> <C-t> :term ++kill=term<CR>
       map <silent> <C-o> :FZF $HOME<CR>
       map <silent> <C-l> :set invnumber<CR>
-      
+
       "" Vim-markdown-preview toggle
       nnoremap mdp :Glow<CR>
-      
+
       command NoComments %s/#.*\n//g
       command GitAdd :w! <bar> !git add -v %
       command GitCommit !$gitmessage = ($gitmessage = read-host -prompt "commit message") && git commit -s -m ''${gitmessage}
       command GitPush !gpsup
-      
+
       nnoremap ga :GitAdd<CR>
       nnoremap gc :GitCommit<CR>
       nnoremap gp :GitPush<CR>
       nnoremap nc :NoComments<CR>
-      
+
       " Folding
       set foldmethod=syntax
       set foldlevel=99
-      
+
       " Show matching characters like paranthesis, brackets, etc.
       set showmatch
-      
+
       " tell vim to keep a backup file
       set backup
       " tell vim where to put its backup files
       set backupdir=~/tmp
       " tell vim where to put swap files
       set dir=~/tmp
-      
+
       " Cursor settings:
-      
+
       "  1 -> blinking block
-      "  2 -> solid block 
+      "  2 -> solid block
       "  3 -> blinking underscore
       "  4 -> solid underscore
       "  5 -> blinking vertical bar
       "  6 -> solid vertical bar
-      
+
       " back to line cursor when in normal mode
       let &t_SI.="\e[5 q" "SI = INSERT mode
       let &t_SR.="\e[5 q" "SR = REPLACE mode
       let &t_EI.="\e[5 q" "EI = NORMAL mode (ELSE)
-      
+
       " Convert tabs to spaces
       set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
-      
+
       " Ignore case with search
       set ignorecase smartcase
 
@@ -398,7 +401,7 @@
       highlight ExtraWhitespace ctermbg=red guibg=red
       match ExtraWhitespace /\s\+$/
     '';
-  
+
     plugins = with pkgs.vimPlugins; [
       ale
       base16-vim
@@ -484,7 +487,7 @@
     enableCompletion = true;
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
-  
+
     # Set variables here because Darwin has a bug with zshenv https://github.com/nix-community/home-manager/issues/1782#issuecomment-777500002
     initExtra = ''
       export EDITOR="vim"
@@ -506,7 +509,7 @@
         check_ssh
         vcs_info
       }
-      
+
       # Set ssh-unlock if it's not already set
       alias | grep -q ssh-unlock || alias ssh-unlock="bw get item 01451f67-ca4d-4912-95b6-af8f016e101f | jq -r .notes | ssh-add -t 4h -"
 
@@ -519,7 +522,7 @@
       # Set vultr-unlock alias
       alias vultr-unlock='export VULTR_API_KEY="$(bw get password 2eb34e09-f5b4-4fc2-9c65-ace7013dd1b4)"'
     '';
-  
+
     # Enable oh-my-zsh
     oh-my-zsh = {
       enable = true;

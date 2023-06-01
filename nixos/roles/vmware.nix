e23f395205db971a9206bbf11ca398f1571d8985
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-06-01 09:33:25 -0600

    removed xf86 driver on aarch64 host
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/roles/vmware.nix
+++
@@ -1,9 +0,0 @@
-# Configuration for VMWare guests
-{ config, pkgs, ... }:
-
-{
-  services.xserver.videoDrivers = with pkgs; [
-    "xorg.xf86videovmware"
-  ];
-  virtualisation.vmware.guest.enable = true;
-}

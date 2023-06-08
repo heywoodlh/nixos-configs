e57b0deb23f7a345c1f0d1792d23638f566037a2
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-06-08 10:14:37 -0600

    updated hydra config
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/roles/nixos/hydra.nix
+++ nixos/roles/nixos/hydra.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 
-{  
+{
   networking.firewall.allowedTCPPorts = [
     3000
   ];
 
   services.hydra = {
     enable = true;
-    hydraURL = "http://nix-nvidia.tailscale:3000";
+    hydraURL = "http://0.0.0.0:3000";
     notificationSender = "hydra@heywoodlh.io";
     buildMachinesFiles = [];
     useSubstitutes = true;

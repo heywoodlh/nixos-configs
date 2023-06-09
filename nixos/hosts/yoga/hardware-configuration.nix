64446f7c9ac3ea3c9e89fac9335a48ecb910b8e9
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-06-08 21:50:21 -0600

    removed hidpi scaling from yoga
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/hosts/yoga/hardware-configuration.nix
+++ nixos/hosts/yoga/hardware-configuration.nix
@@ -38,5 +38,5 @@
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
+  #hardware.video.hidpi.enable = lib.mkDefault true;
 }

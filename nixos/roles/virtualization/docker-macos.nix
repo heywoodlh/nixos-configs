9ff823c29772687f3ea05f68dbb8abe4d358b483
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-06-09 13:12:46 -0600

    removed port 5900 reference
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nixos/roles/virtualization/docker-macos.nix
+++ nixos/roles/virtualization/docker-macos.nix
@@ -7,9 +7,6 @@
       docker-macos = {
         image = "docker.io/sickcodes/docker-osx:naked-auto";
         autoStart = true;
-        ports = [
-          "5900:5900"
-        ];
         volumes = [
           "/media/virtual-machines-2/docker-macos/output.env:/env"
           "/media/virtual-machines-2/docker-macos/image/mac_hdd_ng_auto_monterey.img:/image"
@@ -22,7 +19,6 @@
           GENERATE_SPECIFIC = "true";
           DEVICE_MODEL = "iMacPro1,1";
           MASTER_PLIST_URL = "https://raw.githubusercontent.com/sickcodes/osx-serial-generator/master/config-custom.plist";
-          SCREEN_SHARE_PORT = "5900";
         };
         extraOptions = [
           "--device=/dev/kvm"

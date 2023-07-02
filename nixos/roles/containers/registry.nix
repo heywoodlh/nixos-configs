ec10a1f265ff9da0d9bb348ba5dec33b23040c7c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-07-02 12:01:04 -0600

    using NixOS native dockerRegistry service vs container

--- nixos/roles/containers/registry.nix
+++ nixos/roles/containers/registry.nix
@@ -6,21 +6,9 @@
       5000
     ];
   };
-
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers = {
-      registry = {
-        image = "docker.io/registry:2";
-        autoStart = true;
-        ports = [
-          "5000:5000"
-        ];
-        volumes = [
-          "/opt/registry/data:/var/lib/registry"
-          "/etc/localtime:/etc/localtime:ro"
-        ];
-      };
-    };
+  services.dockerRegistry = {
+    enable = true;
+    port = 5000;
+    listenAddress = "0.0.0.0";
   };
 }

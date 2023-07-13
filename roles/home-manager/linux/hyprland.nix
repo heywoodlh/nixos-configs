2132f9e2cf5294a1ac445015563e800e37338277
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-07-13 12:50:41 -0600

    swaylock nord theme

--- roles/home-manager/linux/hyprland.nix
+++ roles/home-manager/linux/hyprland.nix
@@ -172,13 +172,34 @@ in {
     '';
   };
 
-  # Swaylock
+  # Nord-themed Swaylock
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
     settings = {
-      clock = true;
       image = "${homeDir}/.wallpaper.png";
+      bs-hl-color = "b48eadff";
+      caps-lock-bs-hl-color = "d08770ff";
+      caps-lock-key-hl-color = "ebcb8bff";
+      font = "JetBrainsMono Nerd Font Mono";
+      indicator-radius = "25";
+      indicator-thickness = "10";
+      inside-color = "2e3440ff";
+      inside-clear-color = "81a1c1ff";
+      inside-ver-color = "5e81acff";
+      inside-wrong-color = "bf616aff";
+      key-hl-color = "a3be8cff";
+      layout-bg-color = "2e3440ff";
+      line-uses-ring = true;
+      ring-color = "3b4252ff";
+      ring-clear-color = "88c0d0ff";
+      ring-ver-color = "81a1c1ff";
+      ring-wrong-color = "d08770ff";
+      separator-color = "3b4252ff";
+      text-color = "eceff4ff";
+      text-clear-color = "3b4252ff";
+      text-ver-color = "3b4252ff";
+      text-wrong-color = "3b4252ff";
     };
   };
 

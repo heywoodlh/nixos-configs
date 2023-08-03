8486febc8bc9e195a675c6227b838495aa05a8aa
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-08-03 13:50:25 -0600

    updated wezterm+fish, updated spicetify on MacOS

--- darwin/roles/spicetify.nix
+++ darwin/roles/spicetify.nix
@@ -1,16 +1,15 @@
 { config, pkgs, ... }:
 
+# Nord-themed Spicetify for MacOS
+# Before running this, either:
+# - Install Homebrew
+# - Install Spotify
+
 let
   spicetify_theme = "Sleek";
   spicetify_color_scheme = "nord";
 in {
   homebrew = {
-    taps = [
-      "spicetify/homebrew-tap"
-    ];
-    brews = [
-      "spicetify-cli"
-    ];
     casks = [
       "spotify"
     ];
@@ -20,11 +19,11 @@ in {
   system.activationScripts.postUserActivation.text = ''
     export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
     export themes_cloned="false"
-    git clone --depth=1 https://github.com/spicetify/spicetify-themes.git ~/.config/spicetify/Themes 2>/dev/null || themes_cloned="true"
-    [[ -e ~/Library/Application\ Support/Spotify ]] || open -a Spotify && sleep 5
-    [[ -e ~/.config/spicetify/Backup/login.spa ]] || spicetify backup apply
-    spicetify config current_theme ${spicetify_theme}
-    spicetify config color_scheme ${spicetify_color_scheme}
-    spicetify apply
+    ${pkgs.git}/bin/git clone --depth=1 https://github.com/spicetify/spicetify-themes.git ~/.config/spicetify/Themes 2>/dev/null || themes_cloned="true"
+    [[ -e ~/Library/Application\ Support/Spotify ]] || /usr/bin/open -a Spotify && sleep 5
+    [[ -e ~/.config/spicetify/Backup/login.spa ]] || ${pkgs.spicetify-cli}/bin/spicetify-cli backup apply
+    ${pkgs.spicetify-cli}/bin/spicetify-cli config current_theme ${spicetify_theme}
+    ${pkgs.spicetify-cli}/bin/spicetify-cli config color_scheme ${spicetify_color_scheme}
+    ${pkgs.spicetify-cli}/bin/spicetify-cli apply
   '';
 }

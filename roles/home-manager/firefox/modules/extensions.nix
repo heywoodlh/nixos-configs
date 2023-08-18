9ceedbbe3a5deee2daf16e7328987ca9997c324c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-08-18 08:01:30 -0600

    added multi-account-containers

--- roles/home-manager/firefox/modules/extensions.nix
+++ roles/home-manager/firefox/modules/extensions.nix
@@ -4,6 +4,7 @@ with pkgs.nur.repos.rycee.firefox-addons; [
   darkreader
   gnome-shell-integration
   kristofferhagen-nord-theme
+  multi-account-containers
   noscript
   onepassword-password-manager
   privacy-badger

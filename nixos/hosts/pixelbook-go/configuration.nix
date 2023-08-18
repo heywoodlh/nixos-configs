1202097b44c0a0e1bd850ce72cc3e1b08bef7ba3
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-08-18 17:31:54 -0600

    updated pixelbook-go configuration

--- nixos/hosts/pixelbook-go/configuration.nix
+++ nixos/hosts/pixelbook-go/configuration.nix
@@ -11,7 +11,6 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # Setup keyfile
   boot.initrd.secrets = {

d2e9c9c000c43f9227fc6891e8000b5f9667e282
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-08-28 15:28:51 -0600

    updated cache to do builds

--- nixos/roles/nixos/cache.nix
+++ nixos/roles/nixos/cache.nix
@@ -6,4 +6,33 @@
     enable = true;
     secretKeyFile = "/var/cache-priv-key.pem";
   };
+
+  systemd.timers."nix-cache-build" = {
+    wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "weekly";
+        Unit = "nix-cache-build.service";
+      };
+  };
+  systemd.services."nix-cache-build" = {
+    script = ''
+      set -eu
+      rm -rf /tmp/nixos-configs
+      ${pkgs.git}/bin/git clone https://github.com/heywoodlh/nixos-configs.git /tmp/nixos-configs
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-tools --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-precision --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-nvidia --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-ext-net --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-backups --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-server-intel --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-desktop-intel --impure
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild build --flake /tmp/nixos-configs#nix-steam-deck --impure
+
+      rm -rf /tmp/nixos-configs
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
 }

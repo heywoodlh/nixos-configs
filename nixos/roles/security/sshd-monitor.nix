ffa64ecfaec56d24978f3add739d57b2216d1ad9
Spencer Heywood <github@heywoodlh.io>
2023-10-16 18:42:04 -0600

    updated sshd-monitor to not include nextcloud ssh notifications

--- nixos/roles/security/sshd-monitor.nix
+++ nixos/roles/security/sshd-monitor.nix
@@ -10,7 +10,7 @@ let
     grep_regex_pattern='Failed password|Invalid verification code|Invalid user|Accepted publickey|Accepted password|Accepted keyboard-interactive'
 
     ### Pattern to exclude with `grep -v -E ...`
-    grep_exclude_regex_pattern='root from 100.126.114.23|root from 100.98.176.50'
+    grep_exclude_regex_pattern='root from 100.126.114.23|root from 100.98.176.50|backups from 100.108.106.50'
 
     journalctl -u "''${service}" -n 0 -f | grep --line-buffered -iE "''${grep_regex_pattern}" | grep --line-buffered -ivE "''${grep_exclude_regex_pattern}" | while read line
     do

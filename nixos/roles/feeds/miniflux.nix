72935e22dadbaa3960fd136bff10b1078e5e6330
Spencer Heywood <github@heywoodlh.io>
2024-01-17 16:47:42 -0700

    changed user agent in miniflux

--- nixos/roles/feeds/miniflux.nix
+++ nixos/roles/feeds/miniflux.nix
@@ -9,6 +9,7 @@
     enable = true;
     config = {
       LISTEN_ADDR = "0.0.0.0:2000";
+      HTTP_CLIENT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15";
     };
     adminCredentialsFile = "/opt/miniflux/creds.txt";
   };

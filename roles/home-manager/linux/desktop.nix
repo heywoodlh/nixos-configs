9c1bffb7fc12ed3028e34d38b57203c1b4f9f29d
Spencer Heywood <github@heywoodlh.io>
2024-01-19 08:59:50 -0700

    moved WebCord configuration to Linux

--- roles/home-manager/linux/desktop.nix
+++ roles/home-manager/linux/desktop.nix
@@ -13,6 +13,14 @@ in {
     ../firefox/linux.nix
   ];
 
+  # Webcord Nord theme
+  home.file.".config/WebCord/Themes/nordic.theme.css" = {
+    source = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/orblazer/discord-nordic/bfd1da7e7a9a4291cd8f8c3dffc6a93dfc3d39d7/nordic.theme.css";
+      sha256 = "sha256:13q4ijdpzxc4r9423s51hhcc8wzw3901cafqpnyqxn69vr2xnzrc";
+    };
+  };
+
   # Flatpak support
   services.flatpak = {
     enableModule = true;

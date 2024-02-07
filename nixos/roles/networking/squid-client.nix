507373bbe03e27d41a07dd8b002e777f9562136b
Spencer Heywood <github@heywoodlh.io>
2024-02-06 19:31:09 -0700

    using tailscale dns record for squid

--- nixos/roles/networking/squid-client.nix
+++ nixos/roles/networking/squid-client.nix
@@ -2,9 +2,9 @@
 
 {
   networking.proxy = {
-    default = "http://100.113.9.57:3128";
-    httpProxy = "http://100.113.9.57:3128";
-    httpsProxy = "http://100.113.9.57:3128";
+    default = "http://squid.barn-banana.ts.net:3128";
+    httpProxy = "http://squid.barn-banana.ts.net:3128";
+    httpsProxy = "http://squid.barn-banana.ts.net:3128";
     noProxy = "localhost,127.0.0.1,docker.io,*.docker.io,*.docker.com,*.nixos.org,github.com,githubusercontent.com,ghcr.io,*.ghcr.io,*.tailscale";
   };
 }

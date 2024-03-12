8f8346e25c1e47b895780140fe2d07c3a8fb0b13
Spencer Heywood <github@heywoodlh.io>
2024-03-12 16:00:37 -0600

    updated nextcloud settings

--- nixos/roles/storage/nextcloud.nix
+++ nixos/roles/storage/nextcloud.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 80 ];
@@ -15,6 +15,7 @@
       adminuser = "heywoodlh";
     };
 
+    settings.default_phone_region = "US";
     settings.trusted_domains = [
       "drive.heywoodlh.io"
       "nix-drive"
@@ -26,9 +27,14 @@
       createLocally = true;
     };
     home = "/media/storage/nextcloud";
+    maxUploadSize = "40G";
     phpOptions = {
-      upload_max_filesize = lib.mkForce "40G";
-      post_max_size = lib.mkForce "40G";
+      "opcache.interned_strings_buffer" = "16";
     };
+  };
+  services.postgresqlBackup = {
+    enable = true;
+    startAt = "*-*-* 01:15:00";
+    location = "/opt/nextcloud/db-backups";
   };
 }

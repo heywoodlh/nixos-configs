42463f400d03d67db5b3da41368adc6ae8a2e865
Spencer Heywood <github@heywoodlh.io>
2024-03-22 17:42:41 -0600

    updated xrdp configuration

--- nixos/roles/remote-access/xrdp.nix
+++ nixos/roles/remote-access/xrdp.nix
@@ -6,23 +6,10 @@
   services.xrdp = {
     enable = true;
     openFirewall = true;
+    defaultWindowManager = "gnome-remote-desktop";
   };
 
   environment.systemPackages = with pkgs; [
-    gnome.gnome-session # For XRDP+GNOME
+    gnome.gnome-remote-desktop
   ];
-
-  # GNOME settings through home-manager
-  home-manager.users.heywoodlh = {
-    ## Create startwm.sh for XRDP
-    home.file."startwm.sh".text = ''
-      #!/usr/bin/env bash
-      export DESKTOP_SESSION="gnome"
-      export GDMSESSION="gnome"
-      export XDG_CURRENT_DESKTOP="GNOME"
-      export XDG_SESSION_DESKTOP="gnome"
-      dbus-run-session -- gnome-shell
-    '';
-    home.file."startwm.sh".executable = true;
-  };
 }

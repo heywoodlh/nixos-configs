c03085e5853de567de902c23b276297b40147e87
Spencer Heywood <github@heywoodlh.io>
2024-06-20 17:58:23 -0600

    added nixos-dev config

--- flake.nix
+++ flake.nix
@@ -336,6 +336,30 @@
           }
         ];
       };
+      # Dev VM for running on workstations
+      nixos-dev = nixpkgs.lib.nixosSystem {
+        system = "${linuxSystem}";
+        specialArgs = inputs;
+        modules = [
+          /etc/nixos/hardware-configuration.nix
+          ./nixos/console.nix
+          ./nixos/roles/remote-access/sshd.nix
+          {
+            networking.hostName = "nixos-dev";
+            # Bootloader
+            boot.loader.systemd-boot.enable = true;
+            boot.loader.efi.canTouchEfiVariables = false;
+            # Enable networking
+            networking.networkmanager.enable = true;
+            # Set your time zone.
+            time.timeZone = "America/Denver";
+            # Select internationalisation properties.
+            i18n.defaultLocale = "en_US.utf8";
+            system.stateVersion = "24.05";
+          }
+        ];
+      };
+
       # Used in CI
       nixos-desktop-intel = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";

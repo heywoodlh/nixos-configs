eec5f5c719b59375656f2071149cf9c5c373558c
Spencer Heywood <github@heywoodlh.io>
2024-07-08 17:26:19 -0600

    updated gnome-terminal-fullscreen app

--- home/roles/gnome-terminal-fullscreen.nix
+++ home/roles/gnome-terminal-fullscreen.nix
@@ -1,10 +1,18 @@
 { config, pkgs, ... }:
 
-{
-  home.files.".config/autostart/gnome-terminal-fullscreen.desktop" = {
+let
+  homeDir = config.home.homeDirectory;
+in {
+  home.file.".config/autostart/gnome-terminal-fullscreen.desktop" = {
     enable = true;
     text = ''
-      gnome-terminal --full-screen
+      [Desktop Entry]
+      Name=GNOME Terminal fullscreen
+      Exec=gnome-terminal --full-screen
+      Terminal=false
+      Type=Application
+      Icon=${homeDir}/.icons/snowflake.png
+      Categories=Utility;
     '';
   };
 }

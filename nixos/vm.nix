cc1d34db68f6de7c7da943c728065ed2107c1b24
Spencer Heywood <github@heywoodlh.io>
2024-07-08 20:10:28 -0600

    import gnome-terminal-fullscreen with vm.nix

--- nixos/vm.nix
+++ nixos/vm.nix
@@ -1,19 +1,35 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
     /etc/nixos/hardware-configuration.nix
     ./desktop.nix
     ./roles/remote-access/sshd.nix
+    ./roles/dev/gnome-guest.nix
   ];
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
+
   # Enable networking
   networking.networkmanager.enable = true;
+
   # Set your time zone.
   time.timeZone = "America/Denver";
+
+  # Disable sound
+  sound.enable = lib.mkForce false;
+  services.pipewire = {
+    enable = lib.mkForce false;
+    alsa.enable = lib.mkForce false;
+    alsa.support32Bit = lib.mkForce false;
+    pulse.enable = lib.mkForce false;
+  };
+
+  # Disable bluetooth
+  hardware.bluetooth.enable = lib.mkForce false;
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.utf8";
   system.stateVersion = "24.05";

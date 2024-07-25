81b9642adcad6d0af602e2af177219c945a5c649
Spencer Heywood <github@heywoodlh.io>
2024-07-24 21:38:41 -0600

    added more dockerbins

--- home/modules/docker.nix
+++ home/modules/docker.nix
@@ -27,8 +27,24 @@ let
     mkdir -p ~/share/docker
     ${pkgs.git}/bin/git clone https://github.com/1N3/Sn1per ~/share/docker/sniper &>/dev/null || true
     ${pkgs.docker-client}/bin/docker compose --project-directory=$HOME/share/docker/sniper up
-    mkdir -p ~/share/docker/sniper/data
-    ${pkgs.docker-client}/bin/docker run -v $HOME/share/docker/sniper/data:/data -w /data -it --rm sniper-kali-linux /bin/bash
+    mkdir -p ~/share/docker/sniper/loot
+    ${pkgs.docker-client}/bin/docker run -v $HOME/share/docker/sniper/loot:/usr/share/sniper/loot -w /usr/share/sniper/loot -it --rm sniper-kali-linux /bin/bash
+  '';
+  gscript = pkgs.writeShellScriptBin "gscript" ''
+    ${docker-check} || exit 1
+    mkdir -p ~/share/docker/gscript
+    ${pkgs.docker-client}/bin/docker run -it --rm -v $PWD:/workdir -w /workdir -v $HOME/share/docker/gscript:/root/gscript docker.io/heywoodlh/gscript:1.0.0 $@
+  '';
+  nuclei = pkgs.writeShellScriptBin "nuclei" ''
+    ${docker-check} || exit 1
+    mkdir -p ~/share/docker/nuclei/templates
+    mkdir -p ~/share/docker/nuclei/config
+    ${pkgs.docker-client}/bin/docker run --rm -v $HOME/share/docker/nuclei/templates:/root/nuclei-templates -v $HOME/share/docker/nuclei/config:/root/.config/nuclei -v $PWD:/data -w /data --net host -it docker.io/projectdiscovery/nuclei
+  '';
+  msfconsole = pkgs.writeShellScriptBin "msfconsole" ''
+    ${docker-check} || exit 1
+    mkdir -p ~/share/docker/metasploit/
+    ${pkgs.docker-client}/bin/docker run -it --rm --net=host -v $HOME/share/docker/metasploit/:/root/.msf4 -w /root/session -v "$PWD:/root/session" docker.io/heywoodlh/metasploit msfconsole $args
   '';
 in {
   options = {
@@ -46,7 +62,10 @@ in {
   config = mkIf cfg.enable {
     home.packages = [
       docker-multiarch
+      gscript
       sniper
+      nuclei
+      msfconsole
     ];
   };
 }
